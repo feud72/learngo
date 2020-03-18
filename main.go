1b6ca405e34b5d89dd9768636e828dc949769efe
feud72 <feud72@gmail.com>
2020-03-18 11:59:42 +0900

    #3.7

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-import "time"
-
-func main() {
-	c := make(chan string)
-	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
-	for index, person := range people {
-		fmt.Println("Goroutine index:", index)
-		go isSexy(person, c)
-	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c)
-	}
+type requestResult struct {
+	url    string
+	status string
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person + " is sexy"
+func main() {
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string{"https://www.airbnb.com/", "https://www.google.com/", "https://www.amazon.com/", "https://www.reddit.com/", "https://www.google.com/", "https://soundcloud.com/", "https://www.facebook.com/", "https://www.instagram.com/", "https://academy.nomadcoders.co/"}
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		c <- requestResult{url: url, status: "FAILED"}
+	} else {
+		c <- requestResult{url: url, status: status}
+	}
 }
