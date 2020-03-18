c80f7dad685b3b21fc09e4073d4e25acd9f24d14
feud72 <feud72@gmail.com>
2020-03-18 11:41:48 +0900

    #3.5

--- main.go
+++ main.go
@@ -1,36 +1,22 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
-var errRequestFailed = errors.New("Request Failed")
+import "time"
 
 func main() {
-	var results = make(map[string]string)
-	urls := []string{"https://www.airbnb.com/", "https://www.google.com/", "https://www.amazon.com/", "https://www.reddit.com/", "https://www.google.com/", "https://soundcloud.com/", "https://www.facebook.com/", "https://www.instagram.com/", "https://academy.nomadcoders.co/"}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
+	for index, person := range people {
+		fmt.Println("Goroutine index:", index)
+		go isSexy(person, c)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + " is sexy"
 }
